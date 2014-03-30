01f02438a2276149de3878371be0507cd65aef0e
gruchanet <grucha.net@gmail.com>
2014-03-30 18:02:50 +0200

    infinite scrolling test

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,14 +1,43 @@
-$(window).bindWithDelay('scroll', function() {
-    jQuery(function() {
-        if ($('#infinite-scrolling').size() > 0) {
-            $(window).on('scroll', function() {
-                var more_snippets_url;
-                more_snippets_url = $('.pagination .next_page a').attr('href');
-                if (more_snippets_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-                    $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
-                    $.getScript(more_snippets_url);
+jQuery(function() {
+    var hash, loading_snippets, page_regexp, pushPage;
+    page_regexp = /\d+$/;
+    pushPage = function(page) {
+        History.pushState(null, "InfiniteScrolling | Page " + page, "?page=" + page);
+    };
+    window.preparePagination = function(el) {
+        el.waypoint(function(direction) {
+            var $this, page, page_el;
+            $this = $(this);
+            if (!($this.hasClass('first-page') && direction === 'up')) {
+                page = parseInt($this.data('page'), 10);
+                if (direction === 'up') {
+                    page -= 1;
                 }
-            });
-        }
-    });
-}, 100);
+                page_el = $($('#static-pagination li').get(page));
+                if (!page_el.hasClass('active')) {
+                    $('#static-pagination .active').removeClass('active');
+                    pushPage(page);
+                    return page_el.addClass('active');
+                }
+            }
+        });
+    };
+    hash = window.location.hash;
+    if (hash.match(/page=\d+/i)) {
+        window.location.hash = '';
+        window.location.search = '?page=' + hash.match(/page=(\d+)/i)[1];
+    }
+    if ($('#infinite-scrolling').size() > 0) {
+        preparePagination($('.page-delimiter'));
+        $(window).bindWithDelay('scroll', function() {
+            var more_snippets_url;
+            more_snippets_url = $('#infinite-scrolling .next_page a').attr('href');
+            if (more_snippets_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
+                $('#infinite-scrolling .pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+                $.getScript(more_snippets_url, function() {
+                    return pushPage(more_snippets_url.match(page_regexp)[0]);
+                });
+            }
+        }, 100);
+    }
+});
