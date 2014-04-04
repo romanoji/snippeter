602e827cb10fefe066bfb877279cc863ea31201a
gruchanet <grucha.net@gmail.com>
2014-04-04 19:31:31 +0200

    fix-ups

--- app/assets/javascripts/injects/table_extras.js
+++ app/assets/javascripts/injects/table_extras.js
@@ -1,36 +1,30 @@
-var ready = function() {
-    // Make rows clickable
-    $(".table tr").on('click', function(event) {
-        var target = $(event.target);
-        if (target.is(":not(a)") && target.is(":not(span)")) {
-            var href = $(this).data("link");
-            if (href) {
-                window.location.href = href;
-            }
+// Make rows clickable
+$(document).on('click', '.table tr', function(event) {//$(".table tr").on('click', function(event) {
+    var target = $(event.target);
+    if (target.is(":not(a)") && target.is(":not(span)")) {
+        var href = $(this).data("link");
+        if (href) {
+            window.location.href = href;
         }
-    });
+    }
+});
 
-    $('.toggle-notion').find('.collapser').on('click', function(el) {
-        // Change button icon
-        if ($(this).hasClass('collapsed')) {
-            $(this).find('.glyphicon-circle-arrow-up').first()
-                .removeClass('glyphicon-circle-arrow-up')
-                .addClass('glyphicon-circle-arrow-down');
-        } else {
-            $(this).find('.glyphicon-circle-arrow-down').first()
-                .removeClass('glyphicon-circle-arrow-down')
-                .addClass('glyphicon-circle-arrow-up');
-        }
+$(document).on('click', '.collapser', function(e) {//$('.toggle-notion').find('.collapser').on('click', function(event) {
+    // Change button icon
+    if ($(this).hasClass('collapsed')) {
+        $(this).find('.glyphicon-circle-arrow-up').first()
+            .removeClass('glyphicon-circle-arrow-up')
+            .addClass('glyphicon-circle-arrow-down');
+    } else {
+        $(this).find('.glyphicon-circle-arrow-down').first()
+            .removeClass('glyphicon-circle-arrow-down')
+            .addClass('glyphicon-circle-arrow-up');
+    }
 
-        var snippetRow = $(this).parent().parent();
+    var snippetRow = $(this).parent().parent();
 
-        // Smooth scrolling
-        $('html, body').animate({
-            scrollTop: $(snippetRow).position().top - $(snippetRow).height()
-        }, 1000);
-    });
-};
-
-$(document).ready(ready);
-$(document).on('page:change', ready);
-$(document).on('page:update', ready);
+    // Smooth scrolling
+    $('html, body').animate({
+        scrollTop: $(snippetRow).position().top - $(snippetRow).height()
+    }, 1000);
+});
